4a1f6ebe8b1d9f97809699de51f0fdd52f8fa030
Raluca Elisabeta Rusu <raluca.elisabeta.rusu@gmail.com>
2021-07-04 23:47:28 +0200

    toggle reminder on server

--- src/App.js
+++ src/App.js
@@ -24,31 +24,68 @@ const App = () => {
         return data
     }
 
-    //Add Tasks
-    const addTask = (task) => {
-        const id = Math.floor(Math.random() * 10000) + 1
-        console.log(id)
+    // Fetch Task
+    const fetchTask = async (id) => {
+        const res = await fetch(`http://localhost:5000/tasks/${id}`)
+        const data = await res.json()
+
+        return data
+    }
+
+    //Add Task
+
+    const addTask = async (task) => {
+        const res = await fetch('http://localhost:5000/tasks', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(task),
+        })
+
+        const data = await res.json()
+        setTasks([...tasks, data])
+
+        // const id = Math.floor(Math.random() * 10000) + 1
+        // const newTask = { id, ...task }
+        // setTasks([...tasks, newTask])
     }
 
     // Delete Tasks
 
     const deleteTask = async (id) => {
-        await fetch(`http://localhost:5000/tasks/${id}`, {
+        const res = await fetch(`http://localhost:5000/tasks/${id}`, {
             method: 'DELETE',
         })
-
-        setTasks(tasks.filter((task) => task.id !== id))
+        //We should control the response status to decide if we will change the state or not.
+        res.status === 200
+            ? setTasks(tasks.filter((task) => task.id !== id))
+            : alert('Error Deleting This Task')
     }
 
     //Toggle Reminder
 
-    const toggleReminder = (id) => {
+    const toggleReminder = async (id) => {
+        const taskToToggle = await fetchTask(id)
+        const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+        const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(updTask),
+        })
+
+        const data = await res.json()
+
         setTasks(
             tasks.map((task) =>
-                task.id === id ? { ...task, reminder: !task.reminder } : task
+                task.id === id ? { ...task, reminder: data.reminder } : task
             )
         )
     }
+
     return (
         <div className="container">
             <Header
